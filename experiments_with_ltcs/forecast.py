41094ebcd11198f0f1f91447c66868e7e6164a8a
Ivan Slootweg <i_slootweg.acc@outlook.com>
2024-07-03 10:11:20 +0200

    feature - wise plotting plots now include error plots perform training with mixed modality implement optional switch off cosine lr for optuna adaptable to multi-future forecasts

--- experiments_with_ltcs/forecast.py
+++ experiments_with_ltcs/forecast.py
@@ -69,6 +69,7 @@ def cut_in_sequences(x,seq_len,inc=1,prognosis=1):
         end = start+seq_len
         sequences_x.append(x[start:end])
         sequences_y.append(x[start+prognosis:end+prognosis])
+        # sequences_y.append(x[end:end+prognosis])
     return sequences_x,sequences_y
 
 class TrafficData:
@@ -102,6 +103,7 @@ class TrafficData:
         self.train_y = self.train_y[:,permutation[valid_size + test_size :]]
 
         self.feature_labels = ['Holiday','Temperature','Rain','Snow','Clouds','Weekday','Noon']
+
 
 class OccupancyData:
     def __init__(self,seq_len=32,future=1,batch_size=16):
@@ -149,7 +151,6 @@ class OccupancyData:
         self.test_y = np.concatenate([test0_y,test1_y],axis=1)
         self.feature_labels = ['Temperature', 'Humidity','Light','CO2','HumidityRatio']
 
-
     def read_file(self,filename):
         df = pd.read_csv(filename)                                    
 
@@ -163,6 +164,7 @@ class OccupancyData:
         data_y = df['Occupancy'].values.astype(np.int32)
         return data_x,data_y
             
+
 class CheetahData:
     def __init__(self,seq_len=32,future=1,batch_size=16):
         all_files = sorted([os.path.join("data/cheetah",d) for d in os.listdir("data/cheetah") if d.endswith(".npy")])
@@ -181,7 +183,6 @@ class CheetahData:
         self.valid_x, self.valid_y = self._load_files(valid_files)
         self.feature_labels = [f"vector {i}" for i in range(self.train_x.shape[2])]
 
-
     def _load_files(self,files):
         all_x = []
         all_y = []
@@ -196,17 +197,55 @@ class CheetahData:
 
         return np.stack(all_x,axis=1),np.stack(all_y,axis=1)
 
+
+class NeuronData:
+    def __init__(self,seq_len=32,future=1,batch_size=16):
+        x, y = load_trace()
+
+        self.seq_len = seq_len
+        self.batch_size = batch_size
+        self.future = future
+        """ 
+        x: (20,T) -> transpose to (T,20)
+        train_x after cut : (?) * (32,7)
+        after stack: (T, 32, 7). should be (32,T,7)
+        """
+        x = np.load("data/neurons/activations.npy")
+        x = x.astype(np.float32)
+        x = np.transpose(x)
+        train_x, train_y = cut_in_sequences(x,seq_len=seq_len, inc=2,prognosis=future)
+        self.train_x = np.stack(train_x, axis=1) 
+        self.train_y = np.stack(train_y, axis=1)
+
+        # make train-test-val split
+        total_seqs = self.train_x.shape[1]
+
+        permutation = np.random.RandomState(23489).permutation(total_seqs)
+        valid_size = int(0.1 * total_seqs)
+        test_size = int(0.15 * total_seqs)
+
+        self.valid_x = self.train_x[:,permutation[:valid_size]]
+        self.valid_y = self.train_y[:,permutation[:valid_size]]
+        self.test_x = self.train_x[:,permutation[valid_size : valid_size + test_size]]
+        self.test_y = self.train_y[:,permutation[valid_size : valid_size + test_size]]
+        self.train_x = self.train_x[:,permutation[valid_size + test_size :]]
+        self.train_y = self.train_y[:,permutation[valid_size + test_size :]]
+
+        self.feature_labels = self.feature_labels = [f"neuron {i}" for i in range(self.train_x.shape[2])]
+
+
 def get_database_class(data_base):
     class DataBaseClass(data_base):
         def __init__(self,**kwargs):
             super().__init__(**kwargs)
             dataset_names = ["train_x","train_y","valid_x", "valid_y","test_x","test_y"]
+            train_with_noise = False
             for _name in dataset_names:
                 _array = getattr(self,_name)
                 print(f"{_name} shape: ", str(_array.shape), str(_array.mean()))
                 _array = self.normalize(_array,_name.split("_")[-1])
                 print(f"{_name} post normalisation: ",_array.mean())
-                if "train" in _name:
+                if "train" in _name and train_with_noise:
                     noise = np.random.normal(0, 0.1, _array.shape) 
                     _array = _array + noise
                     setattr(self,_name, _array)
@@ -214,7 +253,6 @@ def get_database_class(data_base):
 
             self.in_features = self.train_x.shape[2]
             self.out_features = self.train_y.shape[2]
-
 
         def get_dataloader(self,subset="train"):
             # dataloader input of shapes [BATCH,series_length,features]
@@ -258,13 +296,14 @@ class ForecastModel:
         self.task = task
         self.model_id = model_id
 
-    def fit(self,trial=None,_data=None,epochs=100,learning_rate=1e-2,cosine_lr=False,in_features=None,out_features=None,model_size=None,optimise=False,gpus=None):
+    def fit(self,trial=None,_data=None,epochs=100,learning_rate=1e-2,cosine_lr=False,model_size=None,optimise=False,gpus=None,mixed_memory=False):
             loss = torch.nn.MSELoss()
             # loss = NMSELoss()
             self.n_epochs = epochs
             self.future = _data.future
             self.feature_labels = _data.feature_labels
-            self.in_features = in_features
+            self.in_features = _data.in_features
+            self.out_features = _data.out_features
             self.mean = _data.mean
             self.std = _data.std
             self.experiment_name = f"{self.task}_{self.model_id}"
@@ -273,8 +312,8 @@ class ForecastModel:
                 self.model_size = model_size
                 if(self.model_type.startswith("ltc")):
                     # wiring = AutoNCP(model_size,out_features)
-                    wiring = FullyConnected(model_size,out_features)
-                    self._model = LTC(in_features,wiring,batch_first=True)
+                    wiring = FullyConnected(model_size,self.out_features)
+                    self._model = LTC(self.in_features,wiring,batch_first=True,mixed_memory=mixed_memory)
                 self.learn = SequenceLearner(self._model,loss,learning_rate=learning_rate,cosine_lr=cosine_lr,
                                 _loaderfunc=_data.get_dataloader,n_iterations=(_data.batch_size * epochs))
                 lr_logger = LearningRateMonitor(logging_interval='step')
@@ -302,8 +341,8 @@ class ForecastModel:
 
             else:
                 learning_rate = trial.suggest_float("learning_rate",1e-4,1e-2)
-                cosine_lr = trial.suggest_int("cosine_lr",0,1)
-                model_size = trial.suggest_int("model_size",int((out_features+3)/2)*2,48,4)
+                cosine_lr = trial.suggest_int("cosine_lr",0,1) if cosine_lr else 0
+                model_size = trial.suggest_int("model_size",self.out_features +3,48,4)
                 checkpoint_callback = ModelCheckpoint(
                     save_top_k=1,
                     monitor="val_loss",
@@ -314,9 +353,10 @@ class ForecastModel:
 
                 self.model_size = model_size
                 if(self.model_type.startswith("ltc")):
-                    print("model size: ", model_size, "out: ", out_features)
-                    wiring = AutoNCP(model_size,out_features)
-                    self._model = LTC(in_features,wiring,batch_first=True)
+                    print("model size: ", model_size, "out: ", self.out_features)
+                    # wiring = AutoNCP(model_size,self.out_features)
+                    wiring = FullyConnected(model_size,self.out_features)
+                    self._model = LTC(self.in_features,wiring,batch_first=True,mixed_memory=mixed_memory)
                 self.learn = SequenceLearner(self._model,loss,learning_rate=learning_rate,cosine_lr=cosine_lr,
                                 _loaderfunc=_data.get_dataloader,n_iterations=(_data.batch_size * epochs))
                 self.trainer = pl.Trainer(
@@ -326,8 +366,7 @@ class ForecastModel:
                     accelerator= 'cpu' if gpus is None else 'gpu',
                     devices=gpus,
                     callbacks=[PyTorchLightningPruningCallback(trial, monitor="val_loss"),checkpoint_callback],
-                    # default_root_dir=f"checkpoints/{self.task}_{self.model_id}",
-                    # enable_checkpointing=True
+
                 )
 
                 hyperparameters = dict(lr=learning_rate, seq_len=_data.seq_len, future=_data.future,
@@ -352,6 +391,7 @@ class ForecastModel:
     def plot_test(self,version="before"):
         y_list = []
         y_hat_list = []
+        error_list = []
 
         for _batch in self.trainer.test_dataloaders:
             # _batch = (B_size, Timesteps, Features)
@@ -360,32 +400,53 @@ class ForecastModel:
             y_hat = y_hat.view_as(y)
             y = self.denormalize(y,"y")
             y_hat = self.denormalize(y_hat,"y")
-            y_list.append(y[:,-1,:])
-            y_hat_list.append(y_hat[:,-1,:])
+            error = y - y_hat
+            y_list.extend(y[:,-self.future:,:])
+            y_hat_list.extend(y_hat[:,-self.future:,:])
+            error_list.extend(error[:,-self.future:,:])
+
         y = torch.cat(y_list,dim=0).detach().numpy()
         y_hat = torch.cat(y_hat_list,dim=0).detach().numpy()
+        error = torch.cat(error_list,dim=0).detach().numpy()
 
-        fig, axes = plt.subplots(self.in_features, 1, figsize=(30,4*self.in_features))
+        fig, axes = plt.subplots(self.in_features, 1, figsize=(30,4*self.in_features),constrained_layout=True)
         axes = axes.ravel()
-        for (feat_nr,ax) in zip(range(self.in_features),axes):
-            ax.plot(y[:,feat_nr],label="Target output",linewidth=1)
-            ax.plot(y_hat[:,feat_nr], label="NCP output",linewidth=1)
-            ax.set_title(f"{self.feature_labels[feat_nr]}",loc = 'left')
-            ax.legend(loc='upper right')
+        for future_point in range(self.future):
+            future_point_indices = torch.LongTensor(list(range(0+future_point,y.shape[0]-self.future +future_point +1,self.future)))
+            y_for_future = np.take(y, future_point_indices, 0)
+            y_hat_for_future = np.take(y_hat,future_point_indices, 0)
+            error_for_future = np.take(error, future_point_indices, 0)
 
-        plt.suptitle(f"{version} training")
-        plt.tight_layout()
-        plt.savefig(f"results/{self.task}_{self.model_id}_{version}.png")
-        plt.close()
+            for (feat_nr,ax) in zip(range(self.in_features),axes):
+                ax.plot(y_for_future[:,feat_nr],label="Target output",linewidth=1)
+                ax.plot(y_hat_for_future[:,feat_nr], label="NCP output",linewidth=1)
+                ax.set_title(f"{self.feature_labels[feat_nr]}",loc = 'left')
+                ax.legend(loc='upper right')
+
+            plt.suptitle(f"{version} training")
+            plt.savefig(f"results/{self.task}_{self.model_id}_{version}_{future_point}.jpg")
+            plt.close()
+
+            fig, axes = plt.subplots(self.in_features, 1, figsize=(30,4*self.in_features),constrained_layout=True)
+            axes = axes.ravel()
+            for (feat_nr,ax) in zip(range(self.in_features),axes):
+                ax.plot(error_for_future[:,feat_nr], label="Prediction error",linewidth=1)
+                ax.set_title(f"{self.feature_labels[feat_nr]}",loc = 'left')
+                ax.legend(loc='upper right')
+
+            plt.suptitle(f"{version} training")
+            plt.savefig(f"results/{self.task}_{self.model_id}_{version}_{future_point}-error.jpg")
+            plt.close()
 
 data_classes = {
     "cheetah": CheetahData,
     "traffic": TrafficData,
-    "occupancy": OccupancyData
+    "occupancy": OccupancyData,
+    "neurons": NeuronData
 }
 
 study_names = {
-    "cheetah":20240626131922,
+    "cheetah":2024070307,
     "occupancy":20240626131851
 }
 
@@ -394,6 +455,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model',default="ltc")
     parser.add_argument('--size',default=32,type=int)
+    parser.add_argument('--mixed_memory',action='store_true')
     parser.add_argument('--epochs',default=200,type=int)
     parser.add_argument('--gpus', nargs='+', type=int,default = None)    
     parser.add_argument('--initial_lr',default=0.02,type=float)
@@ -403,6 +465,7 @@ if __name__ == "__main__":
     parser.add_argument('--future',default=1,type=int)
     parser.add_argument('--optimise',action='store_true')
     parser.add_argument('--pruning',action='store_true')
+    parser.add_argument('--load_previous',type=int,default=0)
 
     args = parser.parse_args()
 
@@ -413,9 +476,10 @@ if __name__ == "__main__":
     task = args.dataset + "_forecast"
     model_id = dt.datetime.today().strftime("%Y%m%d%H")
     
+    
     model = ForecastModel(task=task,model_id = model_id,model_type=args.model)
     if not args.optimise:
-        model.fit(_data=dataset_data,epochs=args.epochs,gpus=args.gpus,in_features=dataset_data.in_features, out_features = dataset_data.out_features,optimise=args.optimise,
+        model.fit(_data=dataset_data,epochs=args.epochs,gpus=args.gpus,optimise=args.optimise,mixed_memory=args.mixed_memory,
                 learning_rate=args.initial_lr,cosine_lr=args.cosine_lr,model_size=args.size)
         model.test()
     
@@ -424,16 +488,13 @@ if __name__ == "__main__":
                 url=f"sqlite:///{task}.db",
                 engine_kwargs={"connect_args": {"timeout": 100}},
         )
-
+        
         pruner = optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
         study = optuna.create_study(direction="minimize", pruner=pruner,
-                                    study_name=model_id,
-                                    # study_name = study_names[args.dataset],
-        
+                                    study_name= str(int(model_id)-args.load_previous),
                                     storage=storage,load_if_exists=True)
         study.optimize(lambda trial: model.fit(trial=trial,  
-                                            _data=dataset_data,epochs=args.epochs,gpus=args.gpus,optimise=args.optimise,
-                                            in_features=dataset_data.in_features, out_features = dataset_data.out_features,
+                                            _data=dataset_data,epochs=args.epochs,gpus=args.gpus,optimise=args.optimise,mixed_memory=args.mixed_memory,
                                             learning_rate=args.initial_lr,cosine_lr=args.cosine_lr,model_size=args.size),
                         n_trials=100)
 
